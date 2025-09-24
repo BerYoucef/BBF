0d09b8796de5e3756a1c733a17e2272326bd8c02
BerYoucef <145302187+BerYoucef@users.noreply.github.com>
2025-09-24 17:34:42 +0100

    Add files via upload

---
+++ XSS/Reflected-XSS-P0-002/models/Product-Model.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const {readJsonFile, writeJsonFile} = require('../utils/db');
+
+const db_path = path.join(__dirname, "..", "db_products.json");
+
+function readDB() {
+    return readJsonFile(db_path);
+}
+
+function writeDB(data) {
+    writeJsonFile(db_path, data);
+}
+
+
+module.exports = {readDB, writeDB}
